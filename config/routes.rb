c27f96ddb78d65cbf285660e32aab7f81a6d2044
Cess <cessmbuguar@gmail.com>
2016-06-28 15:55:50 +0300

    create static pages and header partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
