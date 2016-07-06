7470963d954c6120490ff8b7790b1f3e06fda27d
AndyCodez <andystudy4@gmail.com>
2016-07-06 12:49:39 +0300

    Add remember digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :username, presence: true, length: { minimum: 4 }
   validates :email, presence: true, length: { minimum: 11 }, uniqueness: true
   validates :password, length: { minimum: 6 }
   has_secure_password
+
+  #Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #Remembers a user in the database
+  def remember
+    self.remember_token = User.new_token
+    update_attribute( :remember_digest, User.digest(remember_token) )
+  end
 end
